a9f71061d4afbb576199c26615eb7887b85079d4
R Shivaram <85446156+shivaramr@users.noreply.github.com>
2024-04-01 15:38:05 +0530

    Update Footer index.tsx

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  DevicePhoneMobileIcon,
-  EnvelopeIcon,
-  MapIcon,
-} from "@heroicons/react/20/solid";
+import { DevicePhoneMobileIcon, EnvelopeIcon, MapIcon } from "@heroicons/react/20/solid";
 
 const Footer = () => {
   return (
@@ -14,11 +10,9 @@ const Footer = () => {
             <MapIcon className="md:w-[5rem] md:h-[5rem] w-[3.5rem] h-[3.5rem] text-black" />
           </div>
           <div>
-            <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">
-              Address
-            </h1>
-            <p className="text-[17px] w-[50%] text-white opacity-60">
-              Lorem Ipsum dolor sit
+            <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">Address</h1>
+            <p className="text-[16px] w-[100%] text-white opacity-60">
+              Aswathy, Metro Gardens, Kerala-682017
             </p>
           </div>
         </div>
@@ -27,12 +21,8 @@ const Footer = () => {
             <DevicePhoneMobileIcon className="md:w-[5rem] md:h-[5rem] w-[3.5rem] h-[3.5rem] text-black" />
           </div>
           <div>
-            <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">
-              Phone
-            </h1>
-            <p className="text-[17px] w-[50%] text-white opacity-60">
-              +91 - 1234567890
-            </p>
+            <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">Phone</h1>
+            <p className="text-[16px] w-[100%] text-white opacity-60">+91-9567794351</p>
           </div>
         </div>
         <div className="flex items-center space-x-6">
@@ -40,18 +30,14 @@ const Footer = () => {
             <EnvelopeIcon className="md:w-[5rem] md:h-[5rem] w-[3.5rem] h-[3.5rem] text-black" />
           </div>
           <div>
-            <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">
-              Send Us Email
-            </h1>
-            <p className="text-[17px] w-[50%] text-white opacity-60">
-              man@mail.com
-            </p>
+            <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">Send An Email</h1>
+            <p className="text-[16px] w-[50%] text-white opacity-60">shivaramr.mec@gmail.com</p>
           </div>
         </div>
       </div>
       <div className="w-[80%] mt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 items-center justify-between">
         <div className="text-[16px] mb-[2rem] md:mb-0 text-white opacity-20">
-          Channel name `year` | All Rights Reserved
+          Shivaram`s portfolio as on 2024 | All Rights Reserved
         </div>
         <div className="flex items-center space-x-10">
           <p className="text-[16px] text-white opacity-20">Terms & Condition</p>
