dd94d9f452ab1272cf87605237af731889047bf7
R Shivaram <85446156+shivaramr@users.noreply.github.com>
2024-04-01 15:39:43 +0530

    Update My Services index.tsx

--- app/components/MyServices/index.tsx
+++ app/components/MyServices/index.tsx
@@ -15,42 +15,35 @@ const MyServices = () => {
         <div data-aos="fade-right">
           <div className="bg-red-700 hover:scale-110 transform transition-all duration-300 hover:-rotate-6 uppercase font-semibold text-center p-[2rem]">
             <CodeBracketSquareIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae6]" />
-            <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
-              Frontend
-            </h1>
+            <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">Frontend</h1>
             <p className="text-[15px] text-white font-normal">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, enim a assumenda, quam cum laudantium nobis est fuga
-              doloribus, odit iste in ad. Atque autem sint mollitia ad iste
-              dignissimos.
+              Began in Aug 2020 as full-stack web dev with React.js & JS. Transitioned to focus on
+              React.js with TypeScript. Learning Next.js. Exp in Redux, React-Query. Mastering HTML,
+              CSS, SCSS. Explored Material UI, Bootstrap, AntDesign, Syncfusion, Tailwind CSS,
+              DevExtreme for data projects. Excited about DevExtreme`s data features.
             </p>
           </div>
         </div>
         <div data-aos="zoom-in" data-aos-delay="300">
           <div className="bg-orange-700 hover:scale-110 transform transition-all duration-300 uppercase font-semibold text-center p-[2rem]">
             <RocketLaunchIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae6]" />
-            <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
-              Backend
-            </h1>
+            <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">Backend</h1>
             <p className="text-[15px] text-white font-normal">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, enim a assumenda, quam cum laudantium nobis est fuga
-              doloribus, odit iste in ad. Atque autem sint mollitia ad iste
-              dignissimos.
+              Started as a full stack developer, specializing in API development with Node.js,
+              MongoDB, and MySQL. Shifted focus to frontend due to job demands. Currently exploring
+              Nest.js with TypeScript for backend tasks.
             </p>
           </div>
         </div>
         <div data-aos="fade-left" data-aos-delay="500">
           <div className="bg-blue-700 hover:scale-110 transform transition-all duration-300 hover:rotate-6 uppercase font-semibold text-center p-[2rem]">
             <CommandLineIcon className="w-[6rem] h-[6rem] mx-auto text-[#d3fae6]" />
-            <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">
-              Fullstack
-            </h1>
+            <h1 className="text-[20px] md:text-[30px] mt-[1.5rem] mb-[1.5rem]">Fullstack</h1>
             <p className="text-[15px] text-white font-normal">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, enim a assumenda, quam cum laudantium nobis est fuga
-              doloribus, odit iste in ad. Atque autem sint mollitia ad iste
-              dignissimos.
+              Although my career commenced as a full-stack developer, project requirements prompted
+              me to pivot and intensify my focus on frontend development. Presently, I am adept at
+              contributing to both frontend and backend development as per project demands,
+              leveraging my proficiency in JavaScript and TypeScript.
             </p>
           </div>
         </div>
