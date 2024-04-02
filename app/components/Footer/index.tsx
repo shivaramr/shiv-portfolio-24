687ce4224d8018fa07cf15815d4608a41a11ebf5
shivaramr <85446156+shivaramr@users.noreply.github.com>
2024-04-02 15:18:54 +0530

    Eslint Prettier changes

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { DevicePhoneMobileIcon, EnvelopeIcon, MapIcon } from "@heroicons/react/2
 
 const Footer = () => {
   return (
-    <div className="pt-[8rem] pb-[4rem] bg-[#02050a]">
+    <div className="relative z-[30] pt-[8rem] pb-[4rem] bg-[#02050a]">
       <div className="grid border-b-[1px] pb-[6rem] border-gray-400 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 w-[80%] mx-auto gap-[3rem]">
         <div className="flex items-center space-x-6">
           <div className="md:w-[6.5rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
