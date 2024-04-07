b6d7b66d184b85911a43b9d7f794c3f0bca668f1
shivaramr <85446156+shivaramr@users.noreply.github.com>
2024-04-07 16:34:57 +0530

    Update Resume file link

--- app/components/AboutMe/index.tsx
+++ app/components/AboutMe/index.tsx
@@ -24,7 +24,7 @@ const AboutMe = () => {
             </p>
           </div>
           <button className="px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
-            <p>Download CV</p>
+          <a href="/components/resume/R_Shivaram_Resume.pdf" download>Download CV</a>
             <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
           </button>
         </div>
