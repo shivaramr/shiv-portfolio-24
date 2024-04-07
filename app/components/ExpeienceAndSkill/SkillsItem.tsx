1dc2d1fc02f952a2cd21c8b5ba71f27ef3d5628b
R Shivaram <85446156+shivaramr@users.noreply.github.com>
2024-04-07 12:27:37 +0530

    Update SkillsItem.tsx

--- app/components/ExpeienceAndSkill/SkillsItem.tsx
+++ app/components/ExpeienceAndSkill/SkillsItem.tsx
@@ -3,22 +3,25 @@ import React from "react";
 interface Props {
   title: string;
   year: string;
+  data: string;
+  company: string;
+  tech: string;
 }
 
-const SkillsItem = ({ title, year }: Props) => {
+const SkillsItem = ({ title, year, company, data, tech }: Props) => {
   return (
     <div className="mb-[4rem] md:mb-[8rem]">
       <span className="px-[2rem] text-[#55e6a5] py-[0.9rem] font-bold text-[18px] border-[2px] border-[#55e6a5]">
         {year}
       </span>
-      <h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[340x] text-white">
+      <h1 className="mt-[2rem] uppercase font-semibold text-[20px] sm:text-[25px] md:text-[340x] text-white">
         {title}
       </h1>
+      <p className="text-white font-normal mb-[1rem] w-[80%] text-[17px] opacity-78">{company}</p>
+      <p className="text-[#aaaaaa] font-normal mb-[1rem] w-[80%] text-[17px] opacity-80">{data}</p>
       <p className="text-[#aaaaaa] font-normal w-[80%] text-[17px] opacity-80">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto
-        adipisci, sunt, placeat laboriosam natus quia ipsa maiores veniam
-        aspernatur eligendi, inventore ullam cum officia odio corrupti! Quod
-        quibusdam nulla rem!
+        <span className="font-bold me-1">Technologies Used:</span>
+        {tech}
       </p>
     </div>
   );
