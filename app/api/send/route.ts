91f6d24f43e1e7e1151a69cd349affdf8de3baac
shivaramr <85446156+shivaramr@users.noreply.github.com>
2024-04-12 15:14:14 +0530

    Update env variables

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -4,14 +4,15 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL || "";
+const toEmail = process.env.TO_EMAIL || "";
 
 export async function POST(req: Request, res: any) {
   const { email, subject, message } = await req.json();
 
   try {
     const data = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: "shivaramr.mec@gmail.com",
+      from: fromEmail,
+      to: toEmail,
       subject,
       text: "A visitor to your portfolio website has something to say.",
       react: EmailTemplate({ firstName: "John", email, subject, message }),
